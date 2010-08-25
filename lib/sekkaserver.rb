e89f3585fbc267d75f2908f54dd83f54ae03d308
Kiyoka Nishiyama <kiyoka@sumibi.org>
2010-08-25 16:47:31 +0900

    Changed prototype of sekkaserver.

--- lib/sekkaserver.rb
+++ lib/sekkaserver.rb
@@ -1,4 +1,5 @@
 #!/usr/local/bin/ruby
+# -*- coding: utf-8 -*-
 
 require 'rack'
 require 'nendo'
@@ -6,8 +7,28 @@ require 'nendo'
 class SekkaServer
   def initialize
     @core = Nendo::Core.new()
+    @core.loadInitFile
+    @core.evalStr( "(define (nendo_version) *nendo-version*)" )
+    @core.evalStr( "(export-to-ruby nendo_version)" )
+    @core.evalStr( "(define counter 0)" )
+    @core.evalStr( "(define (countup) (begin (set! counter (+ counter 1)) counter))" )
+    @core.evalStr( "(export-to-ruby countup)" )
   end
   def call(env)
-    [200, {"Content-Type" => "text/plain"}, [ "hello, kiyoka!" ]]
+    req = Rack::Request.new(env)
+    body = case req.request_method
+           when 'GET'
+             if req.path == "/count"
+               "counter " + @core.countup.to_s + ":" + req.path
+             else
+               "no message."
+             end
+           end
+    res = Rack::Response.new { |r|
+      r.status = 200
+      r['Content-Type'] = "text/plain"
+      r.write body
+    }
+    res.finish
   end
 end
