3bd8776ccf9f5ea5ea2297839f2f0ed7a7a1ca46
Kiyoka Nishiyama <kiyoka@sumibi.org>
2011-06-20 04:37:01 +0900

    エラー処理: Kvs#put!メソッドで書きこみに失敗したらRuntimeError例外を発生させるようにした。

--- lib/sekka/kvs.rb
+++ lib/sekka/kvs.rb
@@ -1,22 +1,22 @@
 # kvs.rb  -  "wrapper library for Key-Value-Store"
-#  
+#
 #   Copyright (c) 2010  Kiyoka Nishiyama  <kiyoka@sumibi.org>
-#   
+#
 #   Redistribution and use in source and binary forms, with or without
 #   modification, are permitted provided that the following conditions
 #   are met:
-#   
+#
 #   1. Redistributions of source code must retain the above copyright
 #      notice, this list of conditions and the following disclaimer.
-#  
+#
 #   2. Redistributions in binary form must reproduce the above copyright
 #      notice, this list of conditions and the following disclaimer in the
 #      documentation and/or other materials provided with the distribution.
-#  
+#
 #   3. Neither the name of the authors nor the names of its contributors
 #      may be used to endorse or promote products derived from this
 #      software without specific prior written permission.
-#  
+#
 #   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 #   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 #   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -28,8 +28,8 @@
 #   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 #   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 #   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#  
-#  $Id: 
+#
+#  $Id:
 #
 require 'tokyocabinet'
 
@@ -64,7 +64,9 @@ class Kvs
       #p "put! " + key + ":" + value
       case @dbtype
       when :tokyocabinet
-        @db[ key.force_encoding("ASCII-8BIT") ] = value.force_encoding("ASCII-8BIT")
+        if not @db[ key.force_encoding("ASCII-8BIT") ] = value.force_encoding("ASCII-8BIT")
+          raise RuntimeError sprintf( "TokyoCabinet::HDB.put error: key=%s", key.force_encoding("ASCII-8BIT"))
+        end
       else
         raise RuntimeError
       end
--- lib/sekkaconfig.rb
+++ lib/sekkaconfig.rb
@@ -2,24 +2,24 @@
 # -*- coding: utf-8 -*-
 #
 # sekkaconfig.rb  -  "a config info class"
-#  
+#
 #   Copyright (c) 2010  Kiyoka Nishiyama  <kiyoka@sumibi.org>
-#   
+#
 #   Redistribution and use in source and binary forms, with or without
 #   modification, are permitted provided that the following conditions
 #   are met:
-#   
+#
 #   1. Redistributions of source code must retain the above copyright
 #      notice, this list of conditions and the following disclaimer.
-#  
+#
 #   2. Redistributions in binary form must reproduce the above copyright
 #      notice, this list of conditions and the following disclaimer in the
 #      documentation and/or other materials provided with the distribution.
-#  
+#
 #   3. Neither the name of the authors nor the names of its contributors
 #      may be used to endorse or promote products derived from this
 #      software without specific prior written permission.
-#  
+#
 #   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 #   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 #   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -31,15 +31,15 @@
 #   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 #   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 #   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#  
-#  $Id: 
+#
+#  $Id:
 #
 require 'singleton'
 
 module SekkaServer
   class Config
     include Singleton
-    
+
     def self.setup( dictSource, cacheSource = false, listenPort, proxyHost, proxyPort )
       @@dictSource  = dictSource
       @@cacheSource = cacheSource
@@ -47,11 +47,11 @@ module SekkaServer
       @@proxyHost   = proxyHost
       @@proxyPort   = proxyPort
     end
-    
+
     def self.dictSource
       @@dictSource
     end
-    
+
     def self.cacheSource
       @@cacheSource
     end
