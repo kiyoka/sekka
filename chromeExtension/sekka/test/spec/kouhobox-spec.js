d086d46bcf46d1bcc84a4bb1da676b6c9060b1de
Kiyoka Nishiyama <kiyoka@sumibi.org>
2017-04-10 22:12:45 +0900

    返還候補保管庫クラスを作成

---
+++ chromeExtension/sekka/test/spec/kouhobox-spec.js
@@ -0,0 +1,53 @@
+describe('KouhoBox', function () {
+    var kouhobox;
+
+    beforeEach(function () {
+        kouhobox = new KouhoBox(
+            [
+                ["返還", false, "へんかん", 'j', 0],
+                ["変換", false, "へんかん", 'j', 1],
+                ["変化", false, "へんか", 'j', 2]
+            ],
+            "漢字Henkanする",
+            "漢字",
+            "Henkan",
+            "する"
+        )
+    });
+
+    describe('変換候補の返却', function () {
+        it('変換候補リスト', function () {
+            expect(kouhobox.getKouhoList()).toEqual(["返還", "変換", "変化"])
+        });
+        it('次の変換候補(1)', function () {
+            expect(kouhobox.getNextKouho()).toEqual("変換")
+        });
+        it('次の変換候補(2)', function () {
+            expect(kouhobox.getNextKouho()).toEqual("変化")
+        });
+        it('次の変換候補(3)', function () {
+            expect(kouhobox.getNextKouho()).toEqual("返還")
+        });
+    });
+    describe('カーソル位置の状態調査', function () {
+        it('オリジナルテキスト', function () {
+            expect(kouhobox.getOrigText()).toEqual("漢字Henkanする");
+        });
+        it('変換直後の状態か調査(1)', function () {
+            expect(kouhobox.isSelectingPos('漢字返還')).toEqual(true);
+        });
+        it('変換直後の状態か調査(2)', function () {
+            expect(kouhobox.isSelectingPos('漢字変換')).toEqual(true);
+        });
+        it('変換直後の状態か調査(3)', function () {
+            expect(kouhobox.isSelectingPos('漢字変化')).toEqual(true);
+        });
+        it('変換直後の状態か調査(4)', function () {
+            expect(kouhobox.isSelectingPos('漢字Henkan')).toEqual(false);
+        });
+        it('変換直後の状態か調査(5)', function () {
+            expect(kouhobox.isSelectingPos('')).toEqual(false);
+        });
+    });
+
+});
