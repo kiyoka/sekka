f13dc5bc6b7313474bc1d6e3297207ef3e9ea2b3
Kiyoka Nishiyama <kiyoka@sumibi.org>
2017-07-31 22:46:46 +0900

    consoleが無い環境で実行すると、java -jar *.jar で実行する旨のガイダンスを表示するようにした。

--- warbler/bin/init.rb
+++ warbler/bin/init.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../"))
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../app/"))
  
@@ -5,6 +6,20 @@ Dir.glob(File.expand_path('vendor/bundle/jruby/*/gems/*/lib')).each{|path|
   $LOAD_PATH << path if !$LOAD_PATH.include?(path)
 }
  
-require 'app/sekka-server'
- 
-main
+java_import 'java.lang.System'
+java_import 'javax.swing.JOptionPane'
+
+def console_exist?
+  console = System.console()
+  if console.nil?
+    false
+  else
+    true
+  end
+end
+
+if console_exist?
+  require 'app/sekka-server'
+else
+  JOptionPane.showMessageDialog(nil, 'consoleが存在しません。 コンソールで java -jar sekka-server.jar を実行してください')
+end
